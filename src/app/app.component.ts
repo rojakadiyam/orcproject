388a2a22d77c8d60903f1dabd75a52804f0137d3
Roja Mallelu <mallelu.roja@peopletech.com>
2022-03-14 02:28:25 -0700

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'orc';
+author:string='';
+para:string = '';
+img:string='';
+  constructor(){}
+
+  cardData=[
+    {img:'https://dummyimage.com/600x400/000/fff.jpg',author:'Author Name',para:'Some quick example text to build on the card title and make up the bulk of the cards content.'},
+    {img:'https://dummyimage.com/600x400/000/fff.jpg',author:'Author Name',para:'Some quick example text to build on the card title and make up the bulk of the cards content.'},
+    {img:'https://dummyimage.com/600x400/000/fff.jpg',author:'Author Name',para:'Some quick example text to build on the card title and make up the bulk of the cards content.'},
+    {img:'https://dummyimage.com/600x400/000/fff.jpg',author:'Author Name',para:'Some quick example text to build on the card title and make up the bulk of the cards content.'},
+    {img:'https://dummyimage.com/600x400/000/fff.jpg',author:'Author Name',para:'Some quick example text to build on the card title and make up the bulk of the cards content.'},
+     ]
 }
